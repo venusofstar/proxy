e80e728b5e8ef40baf2d8e53b3aa4ba5bf9f17f7
venusofstar <sumidomikeangelo24@gmail.com>
2026-02-07 22:18:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,23 +4,24 @@ const { spawn } = require("child_process");
 const app = express();
 const PORT = 3000;
 
-/**
- * Usage:
- * http://localhost:3000/restream?url=MP4_URL
- */
-app.get("/restream", (req, res) => {
-  const inputUrl = req.query.url;
+// 📌 MP4 SOURCES
+const STREAMS = {
+  gold2: "https://cdn1.magixz.com/Pinaybold/JerrielCry4zeeh2.mp4",
+  gold3: "https://cdn1.magixz.com/Pinaybold/JerrielCry4zeeh3.mp4",
+    gold4: "https://cdn1.magixz.com/Pinaybold/JerrielCry4zeeh4.mp4",
+  gold5: "https://cdn1.magixz.com/Pinaybold/JerrielCry4zeeh5.mp4",
+    gold6: "https://cdn1.magixz.com/Pinaybold/JerrielCry4zeeh6.mp4"
+};
 
-  if (!inputUrl) {
-    return res.status(400).send("Missing MP4 url");
-  }
-
+// reusable restream function
+function restreamMp4(sourceUrl, req, res) {
   res.setHeader("Content-Type", "video/mp4");
   res.setHeader("Transfer-Encoding", "chunked");
+  res.setHeader("Cache-Control", "no-cache");
 
   const ffmpeg = spawn("ffmpeg", [
     "-re",
-    "-i", inputUrl,
+    "-i", sourceUrl,
     "-c:v", "copy",
     "-c:a", "copy",
     "-movflags", "frag_keyframe+empty_moov",
@@ -37,8 +38,44 @@ app.get("/restream", (req, res) => {
   req.on("close", () => {
     ffmpeg.kill("SIGKILL");
   });
+}
+
+// ▶️ Test1 stream
+app.get("/gold2.mp4", (req, res) => {
+  restreamMp4(STREAMS.test1, req, res);
+});
+
+// ▶️ Test2 stream
+app.get("/gold3.mp4", (req, res) => {
+  restreamMp4(STREAMS.test2, req, res);
+});
+
+app.get("/gold4.mp4", (req, res) => {
+  restreamMp4(STREAMS.test1, req, res);
+});
+
+// ▶️ Test2 stream
+app.get("/gold5.mp4", (req, res) => {
+  restreamMp4(STREAMS.test2, req, res);
+});
+
+app.get("/gold6.mp4", (req, res) => {
+  restreamMp4(STREAMS.test1, req, res);
+});
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("MP4 Restream Server Running");
 });
 
 app.listen(PORT, () => {
-  console.log(`MP4 restream server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Test1 → http://localhost:${PORT}/gold1.mp4`);
+  console.log(`Test2 → http://localhost:${PORT}/gold2.mp4`);
+    console.log(`Test1 → http://localhost:${PORT}/gold3.mp4`);
+  console.log(`Test2 → http://localhost:${PORT}/gold4.mp4`);
+    console.log(`Test2 → http://localhost:${PORT}/gold5.mp4`);
 });
